4c8ae3843d7294a90b02dd4324d527e70c5473ae
Q4119-Midnight <tanrivermis.mehmet@gmail.com>
2023-07-17 21:03:31 +0300

    data çağırıldı ve gerekli düzenlemeler yapıldı

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
+import axios from "axios";
 
 const Home = () => {
+  const [tutorials, setTutorials] = useState([]);
+  const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+
+  const getTutorials = async () => {
+    const data = await axios(BASE_URL);
+    setTutorials(data.data);
+  };
+
+  console.log(tutorials);
+
+  //? Mount aşamasında API'ye istek atıyoruz.
+  useEffect(() => {
+    getTutorials();
+  }, []);
+
   return (
     <>
       <AddTutorial />
